093007f209f31d160ed58d1b954654e561b1b94b
EitanLevy1 <eitan2903@gmail.com>
2025-01-27 18:28:43 +0200

    added filter

---
+++ Filter.py
@@ -0,0 +1,22 @@
+from helpers import screen, from_text_to_array
+
+import pygame
+
+from constants import *
+class Filter:
+
+    def __init__(self, color, transparency_level):
+        self.color = color
+        self.transparency_level = transparency_level
+
+    def apply_filter(self):
+        surface = pygame.Surface((POST_WIDTH, POST_HEIGHT))
+        surface.set_alpha(self.transparency_level)
+        surface.fill(self.color)
+        screen.blit(surface, (POST_X_POS, POST_Y_POS))
+
+    def set_filter(self, transparency_level):
+        self.transparency_level = transparency_level
+
+
+
--- ImagePost.py
+++ ImagePost.py
@@ -2,36 +2,28 @@ import pygame
 from constants import *
 from Post import Post
 from helpers import screen, from_text_to_array
-
+from Filter import Filter
 
 
 class ImagePost(Post):
-
-    def __init__(self,image, username, location, description):
+    def __init__(self, image, username, location, description, filter=None):
         super().__init__(username, location, description)
         self.image = image
-
+        self.filter = filter
 
     def display(self, start_comment):
         img = pygame.image.load(self.image)
-        img =pygame.transform.scale(img, (POST_WIDTH, POST_HEIGHT))
+        img = pygame.transform.scale(img, (POST_WIDTH, POST_HEIGHT))
         screen.blit(img, (POST_X_POS, POST_Y_POS))
 
+        if self.filter:
+            self.filter.apply_filter()
         font = pygame.font.SysFont(FONT_NAME, UI_FONT_SIZE)
         text_user = font.render(self.username, True, (0, 0, 0))
         screen.blit(text_user, (USER_NAME_X_POS, USER_NAME_Y_POS))
 
         text_loc = font.render(self.location, True, (0, 0, 0))
         screen.blit(text_loc, (LOCATION_TEXT_X_POS, LOCATION_TEXT_Y_POS))
-
-        # array_desc = from_text_to_array(self.description)
-        # count = 0
-        # pos_y = DESCRIPTION_TEXT_Y_POS
-        # for item in array_desc:
-        #     count += 1
-        #     desc_post = font.render(item, True, BLACK)
-        #     screen.blit(desc_post, (DESCRIPTION_TEXT_X_POS, pos_y))
-        #     pos_y += UI_FONT_SIZE
 
         text_desc = font.render(self.description, True, BLACK)
         screen.blit(text_desc, [DESCRIPTION_TEXT_X_POS, DESCRIPTION_TEXT_Y_POS])
@@ -40,4 +32,3 @@ class ImagePost(Post):
         screen.blit(text_like, (LIKE_TEXT_X_POS, LIKE_TEXT_Y_POS))
 
         self.display_comments(start_comment)
-
