3b3c81b6a255b27309fd57a3490321de3867450c
EitanLevy1 <eitan2903@gmail.com>
2025-02-04 10:29:05 +0200

    updated main

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     posts_data_list = [[img2_post_path, "omerKorin1808", "beer sheva", "love the camel", "image"],
                        [img1_post_path, "noam_belkin", "The father land", "Isagi is legit NPC #MOVE!!!!", purple_filter,"image"],
 
-                  ["We must sign new Wingback! Dalot can't do the job for us!", "mashash", "Manchester", "i am gay", (255, 255, 0), (255, 0, 0), "text"],
+                  ["We must sign new Wingback! Dalot can't do the job for us!", "mashash", "Manchester", "wow", (255, 255, 0), (255, 0, 0), "text"],
                   [img3_post_path, "eitan_levi", "beer sheva", "after a quick goon session", "image"]]
 
     posts_list = []
